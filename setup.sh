31a36eaa0c874b47d40b6bb53abe907132f3162a
Samuel John Makin <SJMakin@users.noreply.github.com>
2023-01-27 20:09:51 +0000

    Add jarvis config

--- setup.sh
+++ setup.sh
@@ -62,6 +62,14 @@ echo 'server {
                 deny all;
         }
 
+        location /jarvis {
+                rewrite ^/jarvis(/.*)$ $1 break;
+                proxy_pass http://127.0.0.1:16000;
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        }
+
         error_page 404 /404/index.html;
         location  /404/index.html {
                 internal;
